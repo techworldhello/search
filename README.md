71e7593dbc19af3a197ffe6bf25d7c6005b3d99d
Jessie Wang <jessie.wang@hotmail.co.uk>
2021-02-17 20:44:16 +1100

    Set up basic app with data files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Search Challenge
+
+A basic CLI app that searches JSON data and outputs the result.
+
+### Caveats
+
+* Only exact matches are supported (no fuzzy matches)
+* Value can be empty if its corresponding key exists
+* If the data does not exist, the program will inform accordingly
---
+++ pkg/schema/organization.go
@@ -0,0 +1,14 @@
+package schema
+
+// Organization represents the shape of organization data
+type Organization struct {
+	ID            int      `json:"_id"`
+	URL           string   `json:"url"`
+	ExternalID    string   `json:"external_id"`
+	Name          string   `json:"name"`
+	DomainNames   []string `json:"domain_names"`
+	CreatedAt     string   `json:"created_at"`
+	Details       string   `json:"details"`
+	SharedTickets bool     `json:"shared_tickets"`
+	Tags          []string `json:"tags"`
+}
