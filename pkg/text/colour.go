bad1e57f0ee5c4ac195e78fd46d7c0f4a2da5dbe
Jessie Wang <jessie.wang@hotmail.co.uk>
2021-02-19 14:17:24 +1100

    Reset ASCII text colour

--- pkg/text/colour.go
+++ pkg/text/colour.go
@@ -2,27 +2,34 @@ package text
 
 import "fmt"
 
+const colourReset = "\033[0m"
+
 // ColourGreen writes green coloured text to stdout
 func ColourGreen(msg string) {
-	fmt.Println("\033[32m", msg)
+	fmt.Println("\033[32m", msg, colourReset)
 }
 
 // ColourYellow writes yellow coloured text to stdout
 func ColourYellow(msg string) {
-	fmt.Println("\u001B[33m", msg)
+	fmt.Println("\u001B[33m", msg, colourReset)
 }
 
 // ColourCyan writes cyan coloured text to stdout
 func ColourCyan(msg string) {
-	fmt.Println("\033[36m", msg)
+	fmt.Println("\033[36m", msg, colourReset)
 }
 
 // ColourPurple writes purple coloured text to stdout
 func ColourPurple(msg string) {
-	fmt.Println("\033[35m", msg)
+	fmt.Println("\033[35m", msg, colourReset)
 }
 
 // ColourWhite writes white coloured text to stdout
 func ColourWhite(msg string) {
-	fmt.Println("\033[37m", msg)
+	fmt.Println("\033[37m", msg, colourReset)
+}
+
+// ColourRed writes red coloured text to stdout
+func ColourRed(msg string) {
+	fmt.Println("\033[31m", msg, colourReset)
 }
