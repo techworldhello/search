45437c0deee43b2d16a76c59cec8f81a5119fc48
Jessie Wang <jessie.wang@hotmail.co.uk>
2021-02-20 13:07:32 +1100

    Add comment on Json type

--- pkg/format/json.go
+++ pkg/format/json.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 )
 
+// Json represents the type to render
 type Json struct {}
 
+// Render writes the search result to stdout in JSON format
 func (j Json) Render(result string) {
 	var prettyJSON bytes.Buffer
 
